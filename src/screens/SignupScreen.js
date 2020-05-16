35a6efcde35b5c3943d3dfa273c5a6940f028e8e
Poonamkumar Parekh <59247196+pkArgus@users.noreply.github.com>
2020-05-16 23:17:01 +0530

    Configured boilerplate for all the screens

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const SignupScreen = () => {
+    return (
+        <Text>SignupScreen</Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const TrackListScreen = () => {
+    return (
+        <Text>TrackListScreen</Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackListScreen;
